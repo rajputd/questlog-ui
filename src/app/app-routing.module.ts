54622a98cbd7c10fadf7b9eb1fb3e1f78e67df8f
Dileep Rajput <dileep_rajput@yahoo.com>
2021-01-05 19:56:50 -0500

    Add todolist page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodolistComponent } from './todolist/todolist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todolist', component: TodolistComponent },
+  { path: '', redirectTo: '/todolist', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
