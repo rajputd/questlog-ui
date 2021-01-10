2026f46c891f61b4932f042cc320db97921a159c
Dileep Rajput <dileeprajput686@gmail.com>
2021-01-09 21:21:40 -0500

    Add truncate pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestlogComponent } from './questlog/questlog.component';
 import { QuestComponent } from './questlog/quest/quest.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestlogComponent,
     QuestComponent,
+    TruncatePipe,
   ],
   imports: [
     BrowserModule,
